d00344fa7e4ce18ac1995af887b1e54de9fb7e2a
qianZir <353122784@qq.com>
2022-10-25 17:42:33 +0800

    feat: 新增

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { parseQueryString } from '@/utils/utils.js';
+import VideoPlayer from 'react-video-js-player';
 import styles from './index.css';
 
 export default function () {
@@ -9,7 +10,15 @@ export default function () {
 
   return (
     <div className={styles.normal}>
-      <video controls src={`./${decodeURIComponent(parseQueryString().a)}`}></video>
+      {/* <video controls src={`./${decodeURIComponent(parseQueryString().a)}`}></video> */}
+      <VideoPlayer
+        controls={true}
+        src={`./${decodeURIComponent(parseQueryString().a)}`}
+        width="100%"
+        styles={{
+          width: '100%',
+        }}
+      />
     </div>
   );
 }
